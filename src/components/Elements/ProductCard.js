e72263f3bbc02022f1112eaf712474463aec2b72
Namit Malasi <namitmalasi@gmail.com>
2023-01-21 14:16:57 +0530

    created the rating component

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Rating } from "./Rating";
 
 export const ProductCard = ({ product }) => {
   const {
@@ -36,11 +37,7 @@ export const ProductCard = ({ product }) => {
         </p>
 
         <div className="flex items-center my-2">
-          <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-          <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-          <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-          <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-          <i className="text-lg bi bi-star text-yellow-500 mr-1"></i>
+          <Rating rating={rating} />
         </div>
 
         <p className="flex justify-between items-center">
---
+++ src/components/Elements/Rating.js
@@ -0,0 +1,24 @@
+export const Rating = ({ rating }) => {
+  let ratingArray = new Array(5).fill(false);
+
+  for (let i = 0; i < rating; i++) {
+    ratingArray[i] = true;
+  }
+  return (
+    <>
+      {ratingArray.map((value, index) =>
+        value ? (
+          <i
+            key={index}
+            className="text-lg bi bi-star-fill text-yellow-500 mr-1"
+          ></i>
+        ) : (
+          <i
+            key={index}
+            className="text-lg bi bi-star text-yellow-500 mr-1"
+          ></i>
+        )
+      )}
+    </>
+  );
+};
