a0f9001ae970d63c863c2ad63035de3e4fa5020c
Namit Malasi <namitmalasi@gmail.com>
2023-02-10 13:09:07 +0530

    ES-Lint Fixes

--- src/components/Elements/DropdownLoggedIn.js
+++ src/components/Elements/DropdownLoggedIn.js
@@ -17,7 +17,7 @@ export const DropdownLoggedIn = ({ setDropdown }) => {
       }
     }
     fetchData();
-  });
+  }, []); //eslint-disable-line
   function handleLogout() {
     logout();
     setDropdown(false);
