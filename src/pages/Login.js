779f2553784953e06bceb9bce559f1cb648bd9d6
Namit Malasi <namitmalasi@gmail.com>
2023-02-10 21:05:18 +0530

    Login as a Guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,6 +24,22 @@ export const Login = () => {
       toast.error(error.message);
     }
   }
+
+  async function handleLoginGuest() {
+    email.current.value = "test@test.com";
+    password.current.value = "1234567";
+
+    try {
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value,
+      };
+      const data = await login(authDetail);
+      data.accessToken ? navigate("/products") : toast.error(data);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
   return (
     <main>
       <section>
@@ -71,7 +87,12 @@ export const Login = () => {
           Log In
         </button>
       </form>
-      {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+      <button
+        onClick={handleLoginGuest}
+        className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Login As Guest
+      </button>
     </main>
   );
 };
