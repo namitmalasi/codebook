1aa2b607fd95c7af3e557f7e3fb8a8986ea336bb
Namit Malasi <namitmalasi@gmail.com>
2023-02-10 13:59:07 +0530

    changes fixes

--- src/pages/Cart/components/Checkout.js
+++ src/pages/Cart/components/Checkout.js
@@ -10,8 +10,8 @@ export const Checkout = ({ setCheckout }) => {
 
   const navigate = useNavigate();
 
-  const token = JSON.parse(sessionStorage.getItem("token"));
-  const cbid = JSON.parse(sessionStorage.getItem("cbid"));
+  // const token = JSON.parse(sessionStorage.getItem("token"));
+  // const cbid = JSON.parse(sessionStorage.getItem("cbid"));
 
   useEffect(() => {
     async function fetchData() {
@@ -136,7 +136,7 @@ export const Checkout = ({ setCheckout }) => {
                     type="number"
                     name="month"
                     id="month"
-                    className="inline-block w-20 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
+                    className="w-20 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
                     value="03"
                     disabled
                     required=""
@@ -145,7 +145,7 @@ export const Checkout = ({ setCheckout }) => {
                     type="number"
                     name="year"
                     id="year"
-                    className="inline-block w-20 ml-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
+                    className="w-20 ml-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
                     value="27"
                     disabled
                     required=""
